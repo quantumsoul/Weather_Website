f1e229ed445e3e7fec0707f541c3f35b155036de
quantumsoul <prakhar24012000@gmail.com>
2022-02-12 03:24:20 +0530

    Added humidity to weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (longitude,latitude,callback) =>{
         }else if(body.error){
             callback('unable to find the location.')
         }else{
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. ' + 'Humidity is ' + body.current.humidity + '%.')
         }
     })
 }
